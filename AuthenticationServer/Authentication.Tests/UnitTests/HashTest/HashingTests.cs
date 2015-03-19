c386a26a7241a9174a226e26fd608c3e0a665fc2
Bobby Habib <bobby.habib@dijits.net>
2015-03-19 23:25:17 +0000

    Web Api Server with JWT authentication added.

---
+++ AuthenticationServer/Authentication.Tests/UnitTests/HashTest/HashingTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Dijits.Authentication.Tests.UnitTests.HashTest
+{
+    [TestFixture]
+    public class HashingTests
+    {
+        private string _secretKey = string.Empty;
+
+        [SetUp]
+        public void SetUpTest()
+        {
+
+        }
+
+        /// <summary>
+        /// The test text hashing.
+        /// </summary>
+        [Test]
+        public void TestNonConfidentialTextHashing()
+        {
+            this._secretKey = Helper.Helper.GetHash("SajjadSofia");
+            if (_secretKey == null)
+            {
+                Assert.Fail();
+            }
+
+            Console.WriteLine(_secretKey);
+        }
+
+        /// <summary>
+        /// The test text hashing.
+        /// </summary>
+        [Test]
+        public void TestConfidentialTextHashing()
+        {
+            _secretKey = Helper.Helper.GetHash("RahimZaraRaees");
+            if (_secretKey == null)
+            {
+                Assert.Fail();
+            }
+
+            Console.WriteLine(_secretKey);
+        }
+    }
+}
---
+++ ResourceServer/PricePaid/PricePaid/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System.Web.Http;
+using Dijits.PricePaid;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using PricePaid;
+
+[assembly: OwinStartup(typeof(StartUp))]
+namespace Dijits.PricePaid
+{
+    public class StartUp
+    {
+        public static OAuthBearerAuthenticationOptions OAuthBearerOptions { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            ////OAuthBearerOptions = new OAuthBearerAuthenticationOptions();
+            //////Token Consumption
+            ////app.UseOAuthBearerAuthentication(OAuthBearerOptions);
+
+            var issuer = "http://localhost:37304/";
+            var audience = "099153c2625149bc8ecb3e85e03f0022";
+            var secret = TextEncodings.Base64Url.Decode("IxrAjDoa2FqElO7IhrSrUJELhUckePEPVpaePlS_Xaw");
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audience },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                    }
+                });
+        }
+    }
+}
